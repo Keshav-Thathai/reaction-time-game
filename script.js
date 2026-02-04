257d8149b5e4020d049dfa0acdd74f80c0f2f4c8
Keshav-Thathai <thataikeshav873@gmail.com>
2026-02-04 14:05:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+let startTime;
+let ready = false;
+
+const screen = document.getElementById("screen");
+const startBtn = document.getElementById("start");
+const text = document.getElementById("text");
+startBtn.onclick=()=>{
+    text.innerText="wait for green...";
+    screen.style.background="red";
+    screen.innerText="WAIT";
+    ready=false;
+
+}
+setTimeout(()=>{
+     screen.style.background="green";
+         screen.innerText="tap";
+         startTime=Date.now();
+         ready=true;
+},Math.random() *3000+2000);
+screen.onclick=()=>{
+    if(!ready) return;
+    let time=Date.now()-startTime;
+    if (time < 300) {
+  text.innerText = "Reaction Time: " + time + " ms 🔥 Excellent";
+} else if (time < 400) {
+  text.innerText = "Reaction Time: " + time + " ms 😃 Very Good";
+} else if (time < 500) {
+  text.innerText = "Reaction Time: " + time + " ms 🙂 Good";
+} else {
+  text.innerText = "Reaction Time: " + time + " ms  Keep Practicing";
+}
+ready=false;
+
+};
+    
+
+
+
+
+
+
+
+
